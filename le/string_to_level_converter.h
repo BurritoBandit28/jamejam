495e6d42876ec82f72ebc659d6be9e547af53c47
jambertrista <tris444@icloud.com>
2026-03-26 16:44:44 +0000

    Completed file processing beta
    level.json files should now be able to be inputted and produce a correct level object

---
+++ le/string_to_level_converter.h
@@ -0,0 +1,27 @@
+//
+// Created by tris4 on 26/03/2026.
+//
+
+#ifndef JAMEJAM_STRING_TO_LEVEL_CONVERTER_H
+#define JAMEJAM_STRING_TO_LEVEL_CONVERTER_H
+#include <string>
+#include <list>
+
+#include "level.h"
+#include "tile_layer.h"
+
+class StringToLevelConverter {
+    public:
+    StringToLevelConverter(std::string jsonString) {
+        this->jsonString = jsonString;
+        convert();
+    };
+
+    private:
+    std::string jsonString;
+    Level convert();
+    TileLayer readTileLayer(std::string name, int index);
+};
+
+
+#endif //JAMEJAM_STRING_TO_LEVEL_CONVERTER_H
