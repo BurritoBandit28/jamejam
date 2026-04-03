c589b48c5bf12e55c9223055ebb5fb3918c04ec8
BurritoBandit28 <yansalmon@gmail.com>
2026-04-03 12:13:58 +0100

    audio test

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_AudioStream *stream = NULL;
+static Uint8 *wav_data = NULL;
+static Uint32 wav_data_len = 0;
 
 int main() {
-    ResourceLocation rl1 = ResourceLocation("game", "test");
+    ResourceLocation rl1 = ResourceLocation("base", "test");
     ResourceLocation rl2 = ResourceLocation::empty();
-    rl2.parse("game2:test");
+    rl2.parse("base:music/menu");
 
     std::cout << rl1.toString() << " " << rl2.toString() << std::endl;
 
@@ -23,7 +25,7 @@ int main() {
         printf("SDL_Init failed: %s\n", SDL_GetError());
     }
 
-    if (!SDL_CreateWindowAndRenderer("SDL Window", 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Game Window (WIP TEST BUILD)", 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -31,6 +33,20 @@ int main() {
     if (!window) {
         printf("Window failed to be created: %s\n", SDL_GetError());
     }
+
+    // in future a dedicated audio manager will be made, this is for a test
+    if (!SDL_LoadWAV("./assets/base/sound/music/menu.wav", &spec, &wav_data, &wav_data_len)) {
+        SDL_Log("Couldn't load .wav file: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, NULL, NULL);
+    if (!stream) {
+        SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_ResumeAudioStreamDevice(stream);
 
     bool running = true;
 
@@ -42,6 +58,12 @@ int main() {
                 running = false;
             }
         }
+
+        if (SDL_GetAudioStreamQueued(stream) < (int)wav_data_len) {
+            /* feed more data to the stream. It will queue at the end, and trickle out as the hardware needs more data. */
+            SDL_PutAudioStreamData(stream, wav_data, wav_data_len);
+        }
+
         SDL_SetRenderDrawColor(renderer,255, 255,255,255);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
@@ -49,6 +71,6 @@ int main() {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
+    SDL_free(wav_data);
     return SDL_APP_SUCCESS;
 }
